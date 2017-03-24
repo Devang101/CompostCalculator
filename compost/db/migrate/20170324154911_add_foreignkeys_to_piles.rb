b86e4095d88e19be802b8f4efccd4e9a38200245
Evan Chrisinger <echrisinger@gmail.com>
2017-03-24 11:38:53 -0700

    Added migration to add temp_log_id, turn_log_id FK's, CN_ratio, volume fields to piles table

---
+++ compost/db/migrate/20170324154911_add_foreignkeys_to_piles.rb
@@ -0,0 +1,15 @@
+class AddForeignkeysToPiles < ActiveRecord::Migration[5.0]
+  def up
+    add_reference :piles, :temp_log, index: true, foreign_key: true
+    add_reference :piles, :turn_log, index: true, foreign_key: true
+    add_column :piles, :CN_ratio, :float
+    add_column :piles, :volume, :float
+  end
+
+  def down
+    remove_column :piles, :volume
+    remove_column :piles, :CN_ratio
+    remove_reference :piles, :temp_log
+    remove_reference :piles, :turn_log
+  end
+end
--- compost/db/schema.rb
+++ compost/db/schema.rb
@@ -10,29 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170324002801) do
+ActiveRecord::Schema.define(version: 20170324154911) do
 
-  create_table "piles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "piles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "user_id"
-    t.text     "content",    limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.text     "content",     limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "temp_log_id"
+    t.integer  "turn_log_id"
+    t.float    "CN_ratio",    limit: 24
+    t.float    "volume",      limit: 24
+    t.index ["temp_log_id"], name: "index_piles_on_temp_log_id", using: :btree
+    t.index ["turn_log_id"], name: "index_piles_on_turn_log_id", using: :btree
     t.index ["user_id", "created_at"], name: "index_piles_on_user_id_and_created_at", using: :btree
     t.index ["user_id"], name: "index_piles_on_user_id", using: :btree
   end
 
-  create_table "temp_logs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "temp_logs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "temperature"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
 
-  create_table "turn_logs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "turn_logs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at",      null: false
@@ -41,5 +47,7 @@ ActiveRecord::Schema.define(version: 20170324002801) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
+  add_foreign_key "piles", "temp_logs"
+  add_foreign_key "piles", "turn_logs"
   add_foreign_key "piles", "users"
 end
